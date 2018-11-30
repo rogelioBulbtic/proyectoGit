4d27823dc5ef970af28953aa49c78af394764139
Rogelio Crisanto <rogelio.bultic@gmail.com>
2018-11-29 23:11:23 -0600

    Se debe instanaciar al pajaro

--- src/proyectogit/Pajaro.java
+++ src/proyectogit/Pajaro.java
@@ -20,7 +20,7 @@ public class Pajaro {
     }
 
     {
-        System.out.println("");
+        System.out.println("y canto");
     }
 
 }
--- src/proyectogit/ProyectoGit.java
+++ src/proyectogit/ProyectoGit.java
@@ -15,7 +15,7 @@ public class ProyectoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Pajaro pajaro = new Pajaro();
     }
     
 }
